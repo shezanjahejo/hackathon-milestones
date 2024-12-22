05d63e507b0ae28e864c3b0864bb0930f848a0c6
shezanjahejo <165707868+shezanjahejo@users.noreply.github.com>
2024-12-21 22:49:10 -0800

    Add files via upload

---
+++ Milestone1.2/script.ts
@@ -0,0 +1,10 @@
+const toggleButton = document.getElementById('toggle-skills') as HTMLButtonElement
+const skills = document.getElementById('skills') as HTMLAnchorElement
+
+toggleButton.addEventListener('click', ()=> {
+    if(skills.style.display === 'none') {
+        skills.style.display = 'block'
+    } else {
+        skills.style.display = 'none'
+    }
+});
